82e1cd67eafb66a762a6e8690d25028eb1a5ff22
Al N <anitu@crowdfavorite.com>
2023-02-16 18:56:58 +0200

    cleanup style

--- src/js/components/markdown/copyToClipboard.js
+++ src/js/components/markdown/copyToClipboard.js
@@ -6,7 +6,7 @@ import Copy from '@js/components/micons/copy';
 const CopyToClipboard = function ({ code }) {
   const handleClick = () => navigator.clipboard.writeText(code[0].props.children[0]);
   return (
-    <button type="button" onClick={handleClick} className="code-copy-btn" title="Copy to clipboard">
+    <button type="button" onClick={handleClick} className="copy" title="Copy to clipboard">
       <Copy />
     </button>
   );
